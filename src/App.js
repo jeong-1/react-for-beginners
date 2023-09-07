bf12ddb88a272f83fbfe1e37919d31cc0dfc2ba4
jeong-1 <ljekj5562@gmail.com>
2023-09-07 17:37:19 +0900

    ✨ feat/create-movie-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";  // react-router-dom 사용
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  return (
-    <div>
-      <h1>Welcome back!</h1>
-    </div>
-  );
+  // Router를 사용하여 페이지 이동
+  return <Router>
+    <Switch>
+      <Route path="/movie/:id">
+        <Detail />
+      </Route>
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}>
+        <Home />
+      </Route>
+    </Switch>
+  </Router>;
 }
 
 export default App;
